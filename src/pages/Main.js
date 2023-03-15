eac6ffae5f63588c85c98eb453cadd0568b19f31
yhyem <yoonhyemin0120@gmail.com>
2023-03-15 15:27:36 +0900

    feat: x 버튼 클릭 or 검색어 삭제시 검색결과 삭제

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,25 +5,38 @@ const Main = () => {
   const [searchKeyword, setSearchKeyword] = useState("");
 
   const handleChangeInput = (event) => {
+    if (event.target.value.length <= 0) {
+      return handleReset();
+    }
     setSearchKeyword(event.target.value);
+    console.log(searchKeyword);
+  };
+
+  const handleReset = () => {
+    console.log("Todo: handleReset", searchKeyword);
+    setSearchKeyword("");
   };
 
   return (
     <>
       <header>
-        <h2 class="container">검색</h2>
+        <h2 className="container">검색</h2>
       </header>
-      <div class="container">
-        <form id="search-form-view">
+      <div className="container">
+        <form
+          id="search-form-view"
+          onSubmit={(event) => handleChangeInput(event)}
+          onReset={handleReset}
+        >
           <input
             type="text"
             placeholder="검색어를 입력하세요"
-            autofocus
+            autoFocus
             value={searchKeyword}
             onChange={handleChangeInput}
           />
           {searchKeyword.length > 0 ? (
-            <button type="reset" class="btn-reset"></button>
+            <button type="reset" className="btn-reset"></button>
           ) : null}
         </form>
       </div>
