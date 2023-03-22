efa68692d466871f5040570c7e92a4f34a44cb54
yhyem <yoonhyemin0120@gmail.com>
2023-03-22 10:30:24 +0900

    chore: 안쓰이는 hook 지우기

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./style.css";
 import store from "../assets/Store.js";
 import Header from "../components/Header.js";
